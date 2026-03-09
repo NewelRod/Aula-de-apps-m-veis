6d62eeec1c09832c863d793970a0ec6ee3c74374
Newel <newelrodriguez@gmail.com>
2026-03-09 17:43:39 -0300

    Updated contato.js with corrected code.

--- aplicativos-moveis-manha-G/Aula02/contato.js
+++ aplicativos-moveis-manha-G/Aula02/contato.js
@@ -2,14 +2,13 @@ function enviar(){
     const nome = document.getElementById("nome").value
     const assunto = document.getElementById("msg").value
 
-    if (nome ||assunto ){
+    if (!nome || !assunto) {
         document.getElementById("nome").style.border = "3px solid red"
         document.getElementById("msg").style.border = "3px solid red"
         return
     }
 
-    const mensagem = `Gostaria de entrar em contato! \n\\Nome: ${nome} \nAssunto: ${assunto}`
+    const mensagem = `Gostaria de entrar em contato!\nNome: ${nome}\nAssunto: ${assunto}`
     const msg = encodeURIComponent(mensagem)
-    window.open(`https//wa.me/5541988973694text=${msg}`, '_blank')
-
+    window.open(`https://wa.me/5541988973694?text=${msg}`, '_blank')
 }
